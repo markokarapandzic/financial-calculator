ce8c471f024d7ee695e32ee0abcfe9a6221b7aac
Marko Karapandzic <webdevmastersensei@gmail.com>
2020-08-28 10:46:03 +0200

    Changes Form Type

--- src/js/constants.js
+++ src/js/constants.js
@@ -11,19 +11,19 @@ const MORTGAGE_HTML = `
 <form class="calculator-form">
   <div class="input-text">
     <label for="home-price">Home Price</label>
-    <input id="home-price" type="text" placeholder="$300,000.00">
+    <input id="home-price" type="number" placeholder="$300,000.00">
   </div>
   <div class="input-text">
     <label for="down-payment">Down Payment</label>
-    <input id="down-payment" type="text" placeholder="$20,000">
+    <input id="down-payment" type="number" placeholder="$20,000">
   </div>
   <div class="input-text">
     <label for="term">Loan Term</label>
-    <input id="term" type="text" placeholder="30 years">
+    <input id="term" type="number" placeholder="30 years">
   </div>
   <div class="input-text">
     <label for="interest">Interest Rate</label>
-    <input id="interest" type="text" placeholder="3.00%">
+    <input id="interest" type="number" placeholder="3.00%">
   </div>
 </form>
 <button class="btn-calculate">
@@ -36,15 +36,15 @@ const LOAN_HTML = `
 <form class="calculator-form">
   <div class="input-text">
     <label for="amount">Loan Amount</label>
-    <input id="amount" type="text" placeholder="$100000.00">
+    <input id="amount" type="number" placeholder="$100000.00">
   </div>
   <div class="input-text">
     <label for="term">Loan Term</label>
-    <input id="term" type="text" placeholder="10 years">
+    <input id="term" type="number" placeholder="10 years">
   </div>
   <div class="input-text">
     <label for="interest">Interest Rate</label>
-    <input id="interest" type="text" placeholder="5.00%">
+    <input id="interest" type="number" placeholder="5.00%">
   </div>
 </form>
 <button class="btn-calculate">
@@ -57,15 +57,15 @@ const INTEREST_HTML = `
 <form class="calculator-form">
   <div class="input-text">
     <label for="principal">Starting Principal</label>
-    <input id="principal" type="text" placeholder="$20000.00">
+    <input id="principal" type="number" placeholder="$20000.00">
   </div>
   <div class="input-text">
     <label for="interest">Interest Rate</label>
-    <input id="interest" type="text" placeholder="5.00%">
+    <input id="interest" type="number" placeholder="5.00%">
   </div>
   <div class="input-text">
     <label for="term">Loan Term</label>
-    <input id="term" type="text" placeholder="5 years">
+    <input id="term" type="number" placeholder="5 years">
   </div>
 </form>
 <button class="btn-calculate">
@@ -78,15 +78,15 @@ const RETURN_ON_INVESTMENT_HTML = `
 <form class="calculator-form">
   <div class="input-text">
     <label for="invested">Amount Invested</label>
-    <input id="invested" type="text" placeholder="$1000.00">
+    <input id="invested" type="number" placeholder="$1000.00">
   </div>
   <div class="input-text">
     <label for="returned">Amount Returned</label>
-    <input id="returned" type="text" placeholder="$2500.00">
+    <input id="returned" type="number" placeholder="$2500.00">
   </div>
   <div class="input-text">
     <label for="length">Investment Length</label>
-    <input id="length" type="text" placeholder="2.5">
+    <input id="length" type="number" placeholder="2.5">
   </div>
 </form>
 <button class="btn-calculate">
@@ -99,27 +99,27 @@ const SALARY_HTML = `
 <form class="calculator-form">
   <div class="input-text">
     <label for="amount">Salary Amount</label>
-    <input id="amount" type="text" placeholder="$30 per hour">
+    <input id="amount" type="number" placeholder="$30 per hour">
   </div>
   <div class="input-text">
     <label for="hours">Hours per Week</label>
-    <input id="hours" type="text" placeholder="40">
+    <input id="hours" type="number" placeholder="40">
   </div>
   <div class="input-text">
     <label for="days">Days per Week</label>
-    <input id="days" type="text" placeholder="5">
+    <input id="days" type="number" placeholder="5">
   </div>
   <div class="input-text">
     <label for="holidays">Holidays per Year</label>
-    <input id="holidays" type="text" placeholder="10">
+    <input id="holidays" type="number" placeholder="10">
   </div>
   <div class="input-text">
     <label for="vacations">Vacations per Year</label>
-    <input id="vacations" type="text" placeholder="15">
+    <input id="vacations" type="number" placeholder="15">
   </div>
   <div class="input-text">
     <label for="inflation">Inflation Rate</label>
-    <input id="inflation" type="text" placeholder="2%">
+    <input id="inflation" type="number" placeholder="2%">
   </div>
 </form>
 <button class="btn-calculate">
